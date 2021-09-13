313720dd58908ebd95543f8ec73086af5fd07e95
temujins <mdsifatulislam.rabbi@gmail.com>
2021-09-13 21:06:29 +0600

    initial commit

--- build/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const express = require('express');
-const dotenv = require('dotenv');
-dotenv.config();
-const app = express();
-const port = process.env.PORT;
-app.listen(port, () => {
-    console.log('server is up and running');
-});
