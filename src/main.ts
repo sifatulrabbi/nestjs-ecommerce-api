5d78d562ac4dc4ba645e14d7f0630735f8d3f6e3
temujins <mdsifatulislam.rabbi@gmail.com>
2021-10-25 13:43:40 +0600

    added global prefix path /api/v1

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+    app.setGlobalPrefix('/api/v1');
+
+    await app.listen(5000);
 }
 bootstrap();
