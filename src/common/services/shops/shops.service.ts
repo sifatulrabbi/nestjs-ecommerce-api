8b03fd7cadcb20737e834b39f5801db138d28851
temujins <mdsifatulislam.rabbi@gmail.com>
2021-11-06 08:24:59 +0600

    🩹 fix(users service, shops service) closes #22 - when user shop exists users will get an error while creating a shop - when username exists users will get an error while creating a user

--- src/common/services/shops/shops.service.ts
+++ src/common/services/shops/shops.service.ts
@@ -6,6 +6,7 @@ export class ShopsService {
   async getShops(req: Request, res: Response): Promise<void> {
     try {
       const shops = await shopsModel.find({});
+
       res.status(201).json({ message: 'success', data: [...shops] });
     } catch (err) {
       res.status(500).json({ error: err });
@@ -15,6 +16,7 @@ export class ShopsService {
   async getAShop(req: Request, res: Response): Promise<void> {
     try {
       const shop = await shopsModel.findById(req.params.shopid);
+
       res.status(201).json({ message: 'success', data: shop });
     } catch (err) {
       res.status(500).json({ error: err });
@@ -28,28 +30,39 @@ export class ShopsService {
   ): Promise<void> {
     try {
       const user = res.locals.user;
+      if (user.shopId) {
+        res.status(400).json({ message: 'users cannot have multiple shops' });
+      }
+
       const shop = req.body.shop;
+
       const newShop = new shopsModel({
         ...shop,
         owner: user.name,
         ownerId: user._id,
       });
+
       const createdShop = await newShop.save();
+
       res.locals.shop = createdShop;
       next();
     } catch (err) {
-      res.status(500).json({ error: err });
+      res.status(err.status | 500).json({
+        error: err ? err : 'internal error in shop service',
+      });
     }
   }
 
   async updateShop(req: Request, res: Response): Promise<void> {
     try {
       const shop: IShop = req.body.shop;
+
       const updatedShop = await shopsModel.findByIdAndUpdate(
         req.params.shopid,
         { ...shop },
         { new: true },
       );
+
       res.status(201).json({ message: 'success', data: updatedShop });
     } catch (err) {
       res.status(500).json({ error: err });
@@ -59,6 +72,7 @@ export class ShopsService {
   async deleteShop(req: Request, res: Response): Promise<void> {
     try {
       await shopsModel.findByIdAndRemove(req.params.id);
+
       res.status(201).json({ message: 'shop deleted' });
     } catch (err) {
       res.status(500).json({ error: err });
