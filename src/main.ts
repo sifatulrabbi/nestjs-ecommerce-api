c3d37a21a379d7f48e670abe00be46d35f78bc6b
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2021-11-09 12:24:16 +0600

    depend(): add validator pipe and class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule as v1 } from './api/v1/app.module';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(v1);
   const config = app.get(ConfigService);
 
   const swaggerConfig = new DocumentBuilder()
@@ -14,6 +15,10 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(config.get<number>('database.uri') | 5000);
+  app.useGlobalPipes(
+    new ValidationPipe({ stopAtFirstError: true, always: true }),
+  );
+
+  await app.listen(config.get<number>('port'));
 }
 bootstrap();
